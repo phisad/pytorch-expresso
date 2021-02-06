c137f0d354b3f1238af5b5ccd7b19d497c889cf2
Philipp Sadler <philipp.sadler@gmail.com>
2021-02-06 17:08:52 +0100

    Add time-period logger

--- torchexpresso/callbacks/logging.py
+++ torchexpresso/callbacks/logging.py
@@ -8,24 +8,30 @@ logger = logging.getLogger(__file__)
 
 class RunningLogger(Callback):
 
-    def __init__(self, period_in_seconds: int = 2):
+    def __init__(self, name="running_logger", period_in_seconds: int = 5):
+        super().__init__(name)
         self.log_period_in_seconds = period_in_seconds
-        self.time_epoch_start = None
         self.current_phase = None
         self.current_epoch = None
+        self.time_last_log = None
 
     def on_epoch_start(self, phase, epoch):
         # TODO: Log every n seconds only, increase period with epoch (at least once every 5 minutes / 300 seconds)
         self.current_phase = phase
         self.current_epoch = epoch
-        self.time_epoch_start = datetime.now()
+        self.time_last_log = datetime.now()
 
     def on_step(self, inputs, outputs, labels, mask, loss, step):
-        time_running = self.time_epoch_start - datetime.now()
-        if time_running.seconds % self.log_period_in_seconds:
+        if self.should_log():
             # TODO: Maybe use "even nicer" python format with padding
             logger.info("Running \t [phase: %s] \t [epoch: %s] \t [step: %s]",
                         self.current_phase, self.current_epoch, step)
+            self.time_last_log = datetime.now()
 
     def on_epoch_end(self, epoch):
         pass
+
+    def should_log(self):
+        time_running = datetime.now() - self.time_last_log
+        time_running_seconds = time_running.seconds
+        return time_running_seconds > self.log_period_in_seconds
