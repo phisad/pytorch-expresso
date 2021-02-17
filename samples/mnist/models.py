5baebeb6c844e5a3a03f75f765cb3d01e6126676
Philipp Sadler <philipp.sadler@gmail.com>
2021-02-17 13:11:00 +0100

    Add cnn experiment to sample

--- samples/mnist/models.py
+++ samples/mnist/models.py
@@ -4,12 +4,13 @@ from torch import nn
 
 class FNNClassifier(nn.Module):
 
-    def __init__(self, task):
+    def __init__(self, task, hidden_size=512):
         super(FNNClassifier, self).__init__()
         self.num_classes = task["num_classes"]
         self.image_shape = task["image_shape"]
-        self.predictor = nn.Linear(in_features=self.image_shape[0] * self.image_shape[1] * self.image_shape[2],
-                                   out_features=self.num_classes)
+        self.image_embedding = nn.Linear(in_features=self.image_shape[0] * self.image_shape[1] * self.image_shape[2],
+                                         out_features=hidden_size)
+        self.predictor = nn.Linear(in_features=hidden_size, out_features=self.num_classes)
 
     def prepare(self, x, device):  # noqa
         x = torch.stack(x).to(device)  # stack listing and shift to gpu
@@ -17,4 +18,50 @@ class FNNClassifier(nn.Module):
         return x
 
     def forward(self, x, device):
-        return self.predictor(x)
+        image_embedding = torch.relu(self.image_embedding(x))
+        return self.predictor(image_embedding)
+
+
+class Conv2dBlock(nn.Module):
+
+    def __init__(self, channels, features, kernels, padding):
+        super(Conv2dBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernels, padding=padding)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+        # The standard non-linearity for convolutional neural networks
+        self.relu = nn.ReLU()
+        # Store indicies for deconvolution
+        self.indicies = []
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.relu(x)
+        x, indicies = self.pool(x)
+        self.indicies = indicies
+        return x
+
+
+class CNNClassifier(nn.Module):
+
+    def __init__(self, task, params=None):
+        super(CNNClassifier, self).__init__()
+        self.num_classes = task["num_classes"]
+        self.image_shape = task["image_shape"]
+        self.block1 = Conv2dBlock(self.image_shape[0], 8, 3, 0)
+        self.block2 = Conv2dBlock(8, 16, 3, 0)
+        self.block3 = Conv2dBlock(16, 32, 3, 0)
+        self.predictor = nn.Linear(in_features=32, out_features=self.num_classes)
+
+    def prepare(self, x, device):  # noqa
+        x = torch.stack(x).to(device)  # stack listing and shift to gpu
+        return x
+
+    def forward(self, x, device):
+        # 32 x  1 x 28 x 28
+        x = self.block1(x)
+        # 32 x  8 x 13 x 13
+        x = self.block2(x)
+        # 32 x 32 x  1 x  1
+        x = self.block3(x)
+        image_embedding = torch.squeeze(x)
+        return self.predictor(image_embedding)
--- samples/mnist/training.py
+++ samples/mnist/training.py
@@ -4,13 +4,13 @@ from torchexpresso.runners import Trainer
 from torchexpresso.configs import ExperimentConfigLoader
 
 
-def perform_training():
+def perform_training(experiment_name, dry_run):
     config = ExperimentConfigLoader("configs") \
-        .with_experiment_params(dry_run=True) \
+        .with_experiment_params(dry_run=dry_run) \
         .with_placeholders(checkpoint_dir=os.getenv("CHECKPOINT_DIR")) \
-        .load("classify/classify-digits", comet_user=os.getenv("COMET_USER"))
+        .load(experiment_name, comet_user=os.getenv("COMET_USER"))
     Trainer.from_config(config, "train", "dev").perform()
 
 
 if __name__ == '__main__':
-    perform_training()
+    perform_training("classify/classify-digits-cnn", dry_run=False)
